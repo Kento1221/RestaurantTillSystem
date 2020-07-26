0a6b3a601c02969b006d65d3a27f3c6dfea79490
Kento1221 <45006862+Kento1221@users.noreply.github.com>
2020-07-26 19:56:32 +0200

    Optimization of application

--- TillApp/MainWindow.xaml.cs
+++ TillApp/MainWindow.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using EFDataAccess.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
 using TillApp.Source;
 
 namespace TillApp
@@ -10,25 +13,28 @@ namespace TillApp
 
     public partial class MainWindow : Window
     {
+        public enum DishTypes { Pho, Starters, Curry, Noodles, Kids, Sides }
         RestaurantDatabaseEntities entities = new RestaurantDatabaseEntities();
-        List<Dish> dishList = new List<Dish>();
-        List<Dish> selectedDishList = new List<Dish>();
-        public enum dishType {Starters, Pho, Curry, Noodles, Kids, Sides}
 
+        //Stores all dishes from database
+        List<Dish> dishList = new List<Dish>();
+        //Stores dishes of type X
+        List<Dish> selectedDishList = new List<Dish>();
+        //Stores ordered dishes
+        List<Dish> orderList = new List<Dish>();
+        //Stores dish types
+        DishTypes currentlySelectedType = new DishTypes();
 
         public MainWindow()
         {
             InitializeComponent();
-            
+
             Clock clock = new Clock(Time_Label, Date_Label);
             clock.StartClock();
-            
+
             PopulateListOfDishes();
-            SelectDishes(dishType.Curry);
+            SelectDishes(DishTypes.Starters);
         }
-
-        private void Close_Button_Click(object sender, RoutedEventArgs e) => Application.Current.Shutdown();
-
 
         /// <summary>
         /// Populates the list of dishes with dishes of all types.
@@ -51,43 +57,104 @@ namespace TillApp
                 dishList.Add(_dish);
             }
         }
+
         /// <summary>
         /// Selects a certain type of dish from populated dishList.
         /// </summary>
         /// <param name="dishType">String of dish type.</param>
-        private void SelectDishes(dishType dishType)
+        public void SelectDishes(DishTypes dishType)
         {
-            selectedDishList.Clear();
-            foreach(var dish in dishList)
+            if (dishType != currentlySelectedType)
             {
-                if (dish.Type == dishType.ToString())
-                    selectedDishList.Add(dish);
+                selectedDishList.Clear();
+                ClearButtons();
+
+                foreach (var dish in dishList)
+                    if (dish.Type == dishType.ToString()) selectedDishList.Add(dish);
+
+                PrintButtons();
+                currentlySelectedType = dishType;
             }
-            PrintButtons();
         }
 
         /// <summary>
-        /// Assignes buttons to each element of selectedDishList.
+        /// Creates buttons. Each of them is assigned a dish from selectedDishList.
         /// </summary>
         private void PrintButtons()
         {
-            for (int i = 0; i<selectedDishList.Count;i++)
+            for (int i = 0; i < selectedDishList.Count; i++)
             {
-                Button _button = (Button)FindName("dish_menu_button" + i);
-                if (_button == null) 
-                    throw new System.Exception("_button is NULL");
-                else
-                {
-                    _button.Content = selectedDishList[i].Name;
-                    _button.Visibility = Visibility.Visible;
-                    _button.IsEnabled = true;
-                    selectedDishList[i].Button = _button;
-                }
+                Button button = new Button();
+                button.Name = "Dish_Menu_Button_"+i;
+                button.Click += Menu_Dish_Button_Click;
+                button.IsEnabled = true;
+                button.Visibility = Visibility.Visible;
+                button.Height = 80f;
+                button.Width = 300f;
+                button.Foreground = Brushes.Black;
+                button.Background = Brushes.LightGray;
+                button.Content = selectedDishList[i].Name;
+
+                Dish_Menu_WrapPanel.Children.Add(button);
             }
         }
 
 
-    
+        private void ClearButtons() => Dish_Menu_WrapPanel.Children.Clear();
+
+
+        private void PrintOrderList()
+        {
+            Order_List.Text = String.Empty;
+            foreach (var orderItem in orderList)
+            {
+                Order_List.Text += orderItem.Name+"\n";
+            }
+        }
+
+        #region button click events
+
+        private void Close_Button_Click(object sender, RoutedEventArgs e) => Application.Current.Shutdown();
+
+        private void Maximize_Button_Click(object sender, RoutedEventArgs e) => this.WindowState = WindowState.Normal;
+
+        private void Starters_button_Click(object sender, RoutedEventArgs e) => SelectDishes(DishTypes.Starters);
+
+        private void Pho_button_Click(object sender, RoutedEventArgs e) => SelectDishes(DishTypes.Pho); 
+
+        private void Curry_button_Click(object sender, RoutedEventArgs e) => SelectDishes(DishTypes.Curry);
+
+        private void Noodles_button_Click(object sender, RoutedEventArgs e) => SelectDishes(DishTypes.Noodles);
+
+        private void Kids_button_Click(object sender, RoutedEventArgs e) => SelectDishes(DishTypes.Kids);
+
+        private void Sides_button_Click(object sender, RoutedEventArgs e) => SelectDishes(DishTypes.Sides);
+
+        private void Menu_Dish_Button_Click(object sender, RoutedEventArgs e)
+        {
+            //retrieving index from the button clicked.
+            Button button = (Button)sender;
+            char[] charTab = "Dish_Menu_Button_".ToArray<char>();
+            string name = button.Name.Trim(charTab);
+            //end
+
+            //Adding dish to Order_List text block
+            orderList.Add(selectedDishList[Int32.Parse(name)]);
+            PrintOrderList();
+        }
+
+        #endregion button click events
+
+
+        #region mouse events
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        #endregion mouse events
     }
 
 
---
+++ TillApp/Source/OrderList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TillApp.Source
+{
+    class OrderItem
+    {
+        public int Id { get; }
+        public string Name { get; }
+        public float Price { get; }
+
+        public OrderItem(int id, string name, float price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
+    }
+
+    class OrderList
+    {
+        private List<OrderItem> _OrderList;
+
+        public OrderList()
+        {
+            _OrderList = new List<OrderItem>();
+        }
+
+        public void AddOrderItem(OrderItem orderItem)
+        {
+            _OrderList.Add(orderItem);
+        }
+
+    }
+}
